ba04154d43ef1a6b60c07d7086d99506027a6844
ivanviso123@gmail.com <ivanviso123@gmail.com>
2021-04-19 02:01:39 +0200

    cosas

--- ipgen.py
+++ ipgen.py
@@ -1,10 +1,14 @@
 import ipaddress
 
 def ipgen(network,usedIPs) :
-    hosts=list(ipaddress.ip_network(network).hosts())
-    for ip in hosts :
-        if ip not in usedIPs:
-            return ip # Rompe el bucle
-    return None # Devuelve un valor nulo explicitamente.
-
+    usedipobj=list()
+    for ip in usedIPs :
+        for ip2 in ip :
+            usedipobj.append(ipaddress.ip_address(ip2))
+    print(usedipobj)
+    for ip in ipaddress.ip_network(network).hosts():
+        if ip not in usedipobj:
+            print(int(ip))
+            return int(ip)
+            
 
